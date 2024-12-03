a8de8dda67dc8d0e9e47a224962ab40d9a580918
SnakerRB <dr6887762@gmail.com>
2024-12-03 18:10:27 +0100

    css

--- src/vistas/frm/landing.php
+++ src/vistas/frm/landing.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="../public/css/nav_landing.css">
   <link rel="stylesheet" href="../public/css/landing.css">
+  <link rel="stylesheet" href="../public/css/nav_landing.css">
 </head>
 
 <body>
